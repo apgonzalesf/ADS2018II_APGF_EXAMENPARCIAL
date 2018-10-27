13ebea7fc65a7ba797486a80d46e2b5de0cab6bc
Alejandra <18102635+apgonzalesf@users.noreply.github.com>
2018-10-27 09:20:58 -0500

    Prueba push

---
+++ Pregunta4/src/com/apgf/pregunta4/Alumno.java
@@ -0,0 +1,38 @@
+package com.apgf.pregunta4;
+
+public class Alumno {
+	
+	Double notaPracticas;
+	Double notaExamenParcial ;
+	Double notaExamenFinal ; 
+	Integer nroClasesAsistidas;
+	public Double getNotaPracticas() {
+		return notaPracticas;
+	}
+	public void setNotaPracticas(Double notaPracticas) {
+		this.notaPracticas = notaPracticas;
+	}
+	public Double getNotaExamenParcial() {
+		return notaExamenParcial;
+	}
+	public void setNotaExamenParcial(Double notaExamenParcial) {
+		this.notaExamenParcial = notaExamenParcial;
+	}
+	public Double getNotaExamenFinal() {
+		return notaExamenFinal;
+	}
+	public void setNotaExamenFinal(Double notaExamenFinal) {
+		this.notaExamenFinal = notaExamenFinal;
+	}
+	public Integer getNroClasesAsistidas() {
+		return nroClasesAsistidas;
+	}
+	public void setNroClasesAsistidas(Integer nroClasesAsistidas) {
+		this.nroClasesAsistidas = nroClasesAsistidas;
+	}
+	
+	
+	 
+	
+
+}
